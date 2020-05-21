609c58039328182d30b0624ef990b7298d39d6e2
Tildes <tildesare.cool@gmail.com>
2020-05-21 16:51:47 -0700

    left over files

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Class MainWindow
+    Private Sub button1_Click(sender As Object, e As RoutedEventArgs)
+        MessageBox.Show("hello")
+
+
+    End Sub
+End Class
