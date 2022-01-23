657db24368f33d8dce06c2908d31a6a7a6c84f2c
Tildes <tildesare.cool@gmail.com>
2022-01-22 19:34:16 -0800

    did my first console example in c#. not anything special. The book is thick enough it's making me anxious.

---
+++ The C# Players guide/chapter 3/FirstCSharp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This seems really basic");
+Console.WriteLine("Not sure why I'm even doing it");
+Console.WriteLine("Wait...I don't need a newline character? well that's good.");
+
+string name;
+name = Console.ReadLine();
+Console.WriteLine("Hi " + name);
